46db03da0ed5308594b291892d5ffebfed4b76c2
luis henrique martinez <asrock_21@hotmail.com>
2019-08-09 16:00:22 -0400

    v 1.2

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
