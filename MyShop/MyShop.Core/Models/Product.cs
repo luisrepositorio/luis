e9e9ea32ff7ac1f6f0aac4e8f11e49501105f5f4
luis henrique martinez <asrock_21@hotmail.com>
2019-08-09 11:22:57 -0400

    V 1.1

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+  public  class Product
+    {
+        public string Id { get; set; }
+
+
+        [StringLength(30)]
+        [DisplayName("Nombre Producto")]
+        public string Name { get; set; }
+        public string Descripcion { get; set; }
+
+        [Range(0,100000)]
+        public decimal Precio { get; set; }
+
+
+        public string Categoria { get; set; }
+        public string Image { get; set; }
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+namespace MyShop.DataAccess.InMemory
+{
+   public class ProductRepository
+    {
+        ObjectCache cahce = MemoryCache.Default;
+        List<Product> products;
+        public ProductRepository()
+        {
+            products = cahce["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        public void Commit()
+        {
+            cahce["products"] = products;
+
+        }
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        public void Update(Product product)
+        {
+            Product ProductToUpdate = products.Find(p => p.Id == product.Id);
+            if (ProductToUpdate != null)
+            {
+                ProductToUpdate = product;
+            } else
+            {
+                throw new Exception("Producto no encontrado");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+            Product Product = products.Find(p => p.Id == Id);
+            if (Product != null)
+            {
+                return Product;
+            }
+            else
+            {
+                throw new Exception("Producto no encontrado");
+            }
+        }
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product ProductToDelete = products.Find(p => p.Id == Id);
+            if (ProductToDelete != null)
+            {
+                products.Remove(ProductToDelete);
+            }
+            else
+            {
+                throw new Exception("Producto no encontrado");
+            }
+        }
+
+    }
+}
