73a515b3018b04f00e50825a8f083b3cfebbfe41
luis henrique martinez <asrock_21@hotmail.com>
2019-08-09 16:27:41 -0400

    V 1.3

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -9,10 +9,74 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
         // GET: ProductManager
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                productToEdit.Name = product.Name;
+                productToEdit.Descripcion = product.Descripcion;
+                productToEdit.Categoria = product.Categoria;
+                productToEdit.Precio = product.Precio;
+                productToEdit.Image = product.Image;
+                return RedirectToAction("Index");
+
+            }
         }
     }
 }
