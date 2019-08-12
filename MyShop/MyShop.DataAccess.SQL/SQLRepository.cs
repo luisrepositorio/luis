38d92846fe40174b5fa75993e197d82090855469
luis henrique martinez <asrock_21@hotmail.com>
2019-08-12 16:14:24 -0400

    V 4

--- MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.SQL
 {
-   public class SQLRepository
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
     {
+        IQueryable<T> IRepository<T>.Collection()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Delete(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        T IRepository<T>.Find(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Update(T t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
