4d6e3ed0d1c0027a63ac0ed25957a2eef5b114a1
luis henrique martinez <asrock_21@hotmail.com>
2019-08-12 08:14:52 -0400

    V 3.1 12/08/2019

--- MyShop/MyShop.Core/Models/BaseEntity.cs
+++ MyShop/MyShop.Core/Models/BaseEntity.cs
@@ -6,14 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-   public abstract class BaseEntity
+    class BaseEntity
     {
-        public string Id { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
-        public BaseEntity()
-        {
-            this.Id = Guid.NewGuid().ToString();
-            this.CreatedAt = DateTime.Now;
-        }
     }
 }
